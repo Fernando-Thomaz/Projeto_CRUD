07cc09f1839fed18f30c09aa1f4692331cee70fd
Fernando <fernandotmgomes@gmail.com>
2025-10-21 08:31:59 -0300

    PROJETO CRUD

--- src/connection.py
+++ src/connection.py
@@ -1,14 +1,20 @@
+# importa banco
 import sqlite3
 
+# função para conectar o banco ao back
 def get_connet():
+    # trata erros
     try:
+        # conecta ao banco "controle_usuario.db"
         conexao = sqlite3.connect("controle_usuario.db")
         print("Conexão bem sucedida!")
         return conexao
     
+    # trata erros
     except sqlite3.Error as e:
         print(f"Falha na conexão {e}")
         return None
-    
+
+# so roda se for branch "main"    
 if __name__ == "__main__" :
     get_connet()
